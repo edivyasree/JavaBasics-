9df1dd20216ce6272dd87d55d12909613f81d40f
Kiran Kumar <edivyasree@gmail.com>
2024-01-10 12:33:17 +0530

    loopsiteration

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // int i=0;
+        // while (true){
+        //     if (i==3) 
+        //     {
+        //      i++;   
+        //      continue;
+        //     }
+        //     System.out.println(i);
+        //     i=i+1;
+        //     if(i>5){
+        //         break;
+        //     }
+        // }
+        // for(int i=0;i<=100;i++){
+        //     System.out.println(i);
+        // }
+        for(int i=100;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+}
